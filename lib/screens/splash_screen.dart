3de1155a5502e7df969bb5056eb438ab6bd84ffd
Sanin <sanin9841393780@gmail.com>
2025-12-05 19:21:03 +0545

    Splash

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,33 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+import 'package:myevents/screens/onboarding_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(const Duration(seconds: 1), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => OnboardingScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Image.asset('assets/image/logo.png', height: 300)),
+    );
+  }
+}
