8ea9b530431518f733879109b9188008cfa5c4af
Sanin <sanin9841393780@gmail.com>
2025-12-18 19:46:00 +0545

    updated

--- lib/app.dart
+++ lib/app.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 
 import 'package:myevents/screens/splash_screen.dart';
+import 'package:myevents/theme/theme_data.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: const SplashScreen());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const SplashScreen(),
+      theme: getApplicationTheme(),
+    );
   }
 }
