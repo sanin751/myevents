148c97ffcecde9a288dafcaa2d59ecbaaa7fa44a
Sanin <sanin9841393780@gmail.com>
2025-12-19 18:48:38 +0545

    updated dashboard

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -9,83 +9,51 @@ class DashboardScreen extends StatefulWidget {
 }
 
 class _DashboardPageState extends State<DashboardScreen> {
-  String _selectedMenu = 'Home';
+  int _currentIndex = 0;
+
+  final List<String> _titles = ['Home', 'Bookings', 'Profile', 'Settings'];
+
+  final List<Widget> _screens = const [
+    HomeScreen(),
+    Center(child: Text('Bookings')),
+    Center(child: Text('Profile')),
+    Center(child: Text('Settings')),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    final purple = Color.fromARGB(252, 202, 60, 60);
-    final lightGrey = Colors.grey.shade200;
+    final purple = Color(0xFFD84315);
 
     return Scaffold(
-      appBar: AppBar(title: Text('Dashboard'), backgroundColor: purple),
-      drawer: Drawer(
-        child: SafeArea(
-          child: Column(
-            children: [
-              ListTile(
-                title: Row(
-                  children: [
-                    Image.asset('assets/image/logo.png', height: 60, width: 60),
-                    SizedBox(width: 10),
-                    Text(
-                      'MY Events',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Divider(),
+      appBar: AppBar(
+        title: Text(_titles[_currentIndex]),
+        backgroundColor: purple,
+        centerTitle: true,
+      ),
 
-              _buildDrawerItem('Home', Icons.home, () {
-                setState(() {
-                  _selectedMenu = 'Home';
-                });
-                Navigator.pop(context);
-              }),
+      body: _screens[_currentIndex],
 
-              _buildDrawerItem('Bookings', Icons.book_online, () {}),
-              _buildDrawerItem('Profile', Icons.person, () {}),
-              _buildDrawerItem('Settings', Icons.settings, () {}),
-
-              Spacer(),
-              ListTile(
-                leading: Icon(Icons.logout, color: Colors.red),
-                title: Text('Logout', style: TextStyle(color: Colors.red)),
-                onTap: () {
-                  Navigator.pushReplacementNamed(context, '/login');
-                },
-              ),
-            ],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        type: BottomNavigationBarType.fixed,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book_online),
+            label: 'Bookings',
           ),
-        ),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
       ),
-      body: _buildBody(lightGrey, purple),
     );
-  }
-
-  Widget _buildDrawerItem(String title, IconData icon, VoidCallback onTap) {
-    bool selected = (_selectedMenu == title);
-    return ListTile(
-      leading: Icon(icon, color: selected ? Colors.blue : null),
-      title: Text(
-        title,
-        style: TextStyle(
-          fontWeight: selected ? FontWeight.bold : FontWeight.normal,
-          color: selected ? Colors.blue : null,
-        ),
-      ),
-      onTap: onTap,
-    );
-  }
-
-  Widget _buildBody(Color bgColor, Color accent) {
-    if (_selectedMenu == 'Home') {
-      return HomeScreen();
-    }
-
-    return Center(child: Text('$_selectedMenu '));
   }
 }
