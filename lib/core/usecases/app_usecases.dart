892d29856729b74db350812304e756f7f8abae27
Sanin <sanin9841393780@gmail.com>
2026-01-02 09:29:13 +0545

    make core files

---
+++ lib/core/usecases/app_usecases.dart
@@ -0,0 +1,11 @@
+import 'package:dartz/dartz.dart';
+import 'package:myevents/core/error/failures.dart';
+
+
+abstract interface class UsecaseWithParms<SucessType, Params> {
+  Future<Either<Failure, SucessType>> call(Params params);
+}
+
+abstract interface class UsecaseWithoutParms<SuccessType> {
+  Future<Either<Failure, SuccessType>> call();
+}
