5d024eca982c2521a38f6d4b72364148e2796103
Sanin <sanin9841393780@gmail.com>
2026-01-16 13:13:48 +0545

    check saved user session data

--- lib/features/auth/presentation/pages/splash_screen.dart
+++ lib/features/auth/presentation/pages/splash_screen.dart
@@ -1,28 +1,42 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:myevents/core/services/storage/user_session_service.dart';
 import 'package:myevents/features/auth/presentation/pages/onboarding_screen.dart';
+import 'package:myevents/features/dashboard/presentation/pages/dashboard_screen.dart';
 
-
-
-class SplashScreen extends StatefulWidget {
+class SplashScreen extends ConsumerStatefulWidget {
   const SplashScreen({super.key});
 
   @override
-  State<SplashScreen> createState() => _SplashScreenState();
+  ConsumerState<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends ConsumerState<SplashScreen> {
+  Future<void> _navigateToNext() async {
+    await Future.delayed(const Duration(seconds: 3));
+    if (!mounted) return;
+    // Note: Check if the user is already logged in
+    final userSessionService = ref.read(userSessionServiceProvider);
+    final isLoggedIn = userSessionService.isLoggedIn();
+
+    if (isLoggedIn) {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => DashboardScreen()),
+      );
+    } else {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => OnboardingScreen()),
+      );
+    }
+  }
+
   @override
   void initState() {
     super.initState();
 
-    Timer(const Duration(seconds: 1), () {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => OnboardingScreen()),
-      );
-    });
+    _navigateToNext();
   }
 
   @override
